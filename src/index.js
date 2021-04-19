b71f18ec5b8c6005b88e10c0f406831380dc3bbb
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-19 10:29:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    console.log("DOM has loaded");
 });
-   
+
+document.getElementById("sub_btn").addEventListener("click", function(event){
+    event.preventDefault();
+    let inp= document.getElementById("new-task-description").value;
+    let li = document.getElementById("tasks");
+    console.log("Button Works!!!");
+    console.log(inp);
+    li.innerHTML += `<li> ${inp} <button> X </button> </li>`;
+    console.log("Success"); 
+ });
+
+//document.getElementById("tasks").childNodes[1].childNodes
+
+ document.getElementsByTagName("button").addEventListener("click", function (){
+    ddocument.getElementById("tasks").childNodes.childNodes[1].remove();
+ })
